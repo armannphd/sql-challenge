7e0345e393e5b556fcd932391f7a366e69162394
Armann <115322974+armannphd@users.noreply.github.com>
2023-02-19 16:40:17 -0800

    added keys

--- SQL-Challenge_Armann.sql
+++ SQL-Challenge_Armann.sql
@@ -3,14 +3,15 @@
 DROP TABLE departments CASCADE;
 DROP TABLE employees CASCADE;
 DROP TABLE dept_emp CASCADE;
-DROP TABLE dept_manage CASCADE;
+DROP TABLE dept_manager CASCADE;
 DROP TABLE salaries CASCADE;
 DROP TABLE titles CASCADE;
 
 -- Create a departments table
 CREATE TABLE departments (
-  dept_no VARCHAR NOT NULL,
-  dept_name VARCHAR NOT NULL
+	dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)	
 );
 
 SELECT * FROM departments;
@@ -23,7 +24,9 @@ CREATE TABLE employees(
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	sex VARCHAR NOT NULL,
-	hire_date VARCHAR NOT NULL
+	hire_date VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 SELECT * FROM employees;
@@ -31,7 +34,9 @@ SELECT * FROM employees;
 -- Create a dept_emp table
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 SELECT * FROM dept_emp;
@@ -39,7 +44,9 @@ SELECT * FROM dept_emp;
 -- Create a dept_manager table
 CREATE TABLE dept_manager(
 	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 SELECT * FROM dept_manager;
@@ -47,7 +54,8 @@ SELECT * FROM dept_manager;
 -- Create a salaries table
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
-	salary INT NOT NULL
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 SELECT * FROM salaries;
@@ -55,7 +63,8 @@ SELECT * FROM salaries;
 -- Create a titles table
 CREATE TABLE titles(
 	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
 );
 
 SELECT * FROM titles;
