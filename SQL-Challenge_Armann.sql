e4277e845cc9c6b95a7e0a9c048ee2c5c1666dff
Armann <115322974+armannphd@users.noreply.github.com>
2023-02-19 19:11:19 -0800

    6 answers to data anlaysis

--- SQL-Challenge_Armann.sql
+++ SQL-Challenge_Armann.sql
@@ -85,10 +85,41 @@ FROM employees e
 WHERE hire_date BETWEEN to_date('1986-01-01','YYYY-MM-DD') 
                     AND to_date('1986-12-31','YYYY-MM-DD');
 					
+--List the manager of each department along with their department number,
+--department name, employee number, last name, and first name
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+INNER JOIN departments d ON
+d.dept_no=dm.dept_no
+INNER JOIN employees e ON
+e.emp_no=dm.emp_no;
 
-					
-					
+--List the department number for each employee along with that employee’s employee number,
+--last name, first name, and department name
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+INNER JOIN employees e ON
+de.emp_no=e.emp_no
+INNER JOIN departments d ON
+d.dept_no=de.dept_no;
 
+--List first name, last name, and sex of each employee whose first name is Hercules and 
+--whose last name begins with the letter B
+SELECT first_name, last_name, sex 
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List each employee in the Sales department, including their
+--employee number, last name, and first name
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE emp_no IN
+(
+	SELECT emp_no
+	FROM dept_emp
+	WHERE dept_no = 'd007'
+);
 
 
 
