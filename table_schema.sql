3b4a384d4863a0efb5c8d0a4b72160c88cf54315
Armann <115322974+armannphd@users.noreply.github.com>
2023-02-19 20:23:10 -0800

    removed two files, update others

---
+++ table_schema.sql
@@ -0,0 +1,72 @@
+-- remove any existing tables
+
+DROP TABLE departments CASCADE;
+DROP TABLE titles CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE salaries CASCADE;
+
+
+
+-- Create a departments table
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)	
+);
+
+SELECT * FROM departments;
+
+-- Create a titles table
+CREATE TABLE titles(
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+SELECT * FROM titles;
+
+-- Create a employees table
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL DEFAULT CURRENT_DATE,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL DEFAULT CURRENT_DATE,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+SELECT * FROM employees;
+
+-- Create a dept_emp table
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+-- Create a dept_manager table
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM dept_manager;
+
+-- Create a salaries table
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries;
