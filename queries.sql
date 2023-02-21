7b0b00ed6ac546b37e21bccf3951f595c362bd71
Armann <115322974+armannphd@users.noreply.github.com>
2023-02-20 16:01:41 -0800

    tidied up code

--- queries.sql
+++ queries.sql
@@ -7,10 +7,9 @@ e.emp_no=s.emp_no;
 
 -- List the first name, last name, and hire date
 -- for the employees who were hired in 1986 
-SELECT e.first_name, e.last_name, e.hire_date 
+SELECT e.first_name, e.last_name, e.hire_date
 FROM employees e
-WHERE hire_date BETWEEN to_date('1986-01-01','YYYY-MM-DD') 
-                    AND to_date('1986-12-31','YYYY-MM-DD');
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'; 
 					
 --List the manager of each department along with their department number,
 --department name, employee number, last name, and first name
@@ -32,46 +31,40 @@ d.dept_no=de.dept_no;
 
 --List first name, last name, and sex of each employee whose first name is Hercules and 
 --whose last name begins with the letter B
-SELECT first_name, last_name, sex 
-FROM employees
+SELECT e.first_name, e.last_name, e.sex
+FROM employees e
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
 --List each employee in the Sales department, including their
 --employee number, last name, and first name
-SELECT emp_no, last_name, first_name
-FROM employees
-WHERE emp_no IN
-(
-	SELECT emp_no
-	FROM dept_emp
-	WHERE dept_no = 'd007'
-);
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+WHERE emp_no IN(
+	SELECT emp_no FROM dept_emp de
+	WHERE de.dept_no IN(
+		SELECT dept_no FROM departments d
+		WHERE d.dept_name = 'Sales'
+));
+
 
 --List each employee in the Sales and Development departments, including their employee number,
 --last name, first name, and department name
-CREATE VIEW joined AS
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
-INNER JOIN dept_emp de ON
-de.emp_no=e.emp_no
-INNER JOIN departments d ON
-d.dept_no=de.dept_no;
+	JOIN dept_emp de ON
+	e.emp_no=de.emp_no
 
-SELECT * FROM joined;
-
-SELECT emp_no, last_name, first_name, dept_name
-FROM joined
-WHERE dept_name = 'Development' OR dept_name = 'Sales';
-
-DROP VIEW joined;
+	JOIN departments d ON
+	d.dept_no=de.dept_no
+	
+	WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 --List the frequency counts, in descending order, of all the employee last names
 --(that is, how many employees share each last name)
-SELECT last_name, COUNT(last_name) AS "last name count"
-FROM employees
-GROUP BY last_name
+SELECT e.last_name, COUNT(last_name) AS "last name count"
+FROM employees e
+GROUP BY e.last_name
 ORDER BY "last name count" DESC;
-
 
 
